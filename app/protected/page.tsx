3f80f2d526bf6f6b34b7715871dd9e3c41280a17
Tiago Miguel <66640308+tiagomiguel29@users.noreply.github.com>
2024-09-08 12:47:12 +0100

    Removed unused imports

--- app/protected/page.tsx
+++ app/protected/page.tsx
@@ -1,6 +1,4 @@
-import FetchDataSteps from "@/components/tutorial/fetch-data-steps";
 import { createClient } from "@/utils/supabase/server";
-import { InfoIcon } from "lucide-react";
 import { redirect } from "next/navigation";
 
 export default async function ProtectedPage() {
