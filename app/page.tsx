38d87398fff66eb0517b746eadb4323dc5d04eba
Tiago Miguel <66640308+tiagomiguel29@users.noreply.github.com>
2024-09-09 11:09:37 +0100

    Corrected text

--- app/page.tsx
+++ app/page.tsx
@@ -50,7 +50,6 @@ export default async function Index() {
 
   return (
     <>
-      <Hero />
       <main className="flex-1 flex flex-col gap-6 px-4 py-8 md:px-8 md:py-12">
         <div className="container mx-auto grid gap-8 md:gap-12">
           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 md:gap-6">
@@ -117,7 +116,7 @@ export default async function Index() {
                   </CardHeader>
                   <CardContent>
                     <p className="text-muted-foreground text-sm">
-                      Submetido em{" "}
+                      Submetida em{" "}
                       {new Date(i.submission_date).toLocaleDateString()}
                     </p>
                   </CardContent>
