3c69026b79f0669d899c54edd9f96708182fb888
Tiago Miguel <66640308+tiagomiguel29@users.noreply.github.com>
2024-09-10 11:48:12 +0100

    Added initiative href

--- app/page.tsx
+++ app/page.tsx
@@ -135,7 +135,7 @@ export default async function Index() {
                   <CardFooter>
                     <div className="flex flex-row-reverse w-full">
                       <Link
-                        href="#"
+                        href={`/iniciativas/${i.id}`}
                         className="inline-flex h-9 items-center justify-center rounded-md bg-primary px-4 text-sm font-medium text-primary-foreground shadow transition-colors hover:bg-primary/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50"
                         prefetch={false}
                       >
