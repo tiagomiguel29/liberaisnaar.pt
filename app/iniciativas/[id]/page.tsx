bbeefce701e471d9430bcb4ab7dc1b0c4343fe96
Tiago Miguel <66640308+tiagomiguel29@users.noreply.github.com>
2024-09-10 18:32:13 +0100

    Fixed width

--- app/iniciativas/[id]/page.tsx
+++ app/iniciativas/[id]/page.tsx
@@ -45,7 +45,7 @@ export default async function InitiativeDetailsPage({
 
   const initiative: ExtendedInitiative = initiativeRes.data;
   return (
-    <main className="flex-1 flex flex-col gap-6 px-4 py-8 md:px-8 md:py-12">
+    <main className="flex-1  w-full md:w-3/4 lg:w-2/3 xl:w-2/3 flex flex-col gap-6 px-4 py-8 md:px-8 md:py-12">
       <div className="container mx-auto grid gap-8 md:gap-12">
         <div>
           <h2 className="text-2xl font-bold mb-4">Detalhes Iniciativa</h2>
