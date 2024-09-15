997c8a1a3411aa6b56c4a4039781c6795ccd6e67
Tiago Miguel <66640308+tiagomiguel29@users.noreply.github.com>
2024-09-15 18:03:50 +0100

    Added toast messages to notification settings

--- app/account/notifications.client.tsx
+++ app/account/notifications.client.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import { Button } from "@/components/ui/button";
 import {
   Card,
@@ -10,62 +10,96 @@ import {
 } from "@/components/ui/card";
 import { Checkbox } from "@/components/ui/checkbox";
 
-import { Tables } from "@/types/database.types"
+import { Tables } from "@/types/database.types";
 import { createClient } from "@/utils/supabase/client";
 import { useState } from "react";
+import toast from "react-hot-toast";
 
-type NotificationSettings = Tables<"notification_settings">
+type NotificationSettings = Tables<"notification_settings">;
 
-export const NotificationSettings = ({notificationSettings} : {notificationSettings: NotificationSettings}) => {
-    const supabase = createClient();
+export const NotificationSettings = ({
+  notificationSettings,
+}: {
+  notificationSettings: NotificationSettings;
+}) => {
+  const supabase = createClient();
 
-    const [settings, setSettings] = useState<NotificationSettings>(notificationSettings)
+  const [settings, setSettings] =
+    useState<NotificationSettings>(notificationSettings);
 
-    async function saveSettings() {
-        const { error } = await supabase.from("notification_settings").upsert(settings).eq("user_id", settings.user_id).single();
-        if (error) {
-            console.error(error);
+  async function saveSettingsReq() {
+    return new Promise(async (resolve, reject) => {
+      const { error } = await supabase
+        .from("notification_settings")
+        .upsert(settings)
+        .eq("user_id", settings.user_id)
+        .single();
+      if (error) {
+        console.error(error);
+        reject(new Error("Error saving settings."));
+      } else {
+        resolve(true);
+      }
+    });
+  }
 
-            // TODO: Show error message
-        }
+  async function saveSettings() {
+    toast.promise(saveSettingsReq(), {
+      loading: "A guardar...",
+      success: "Definições atualizadas.",
+      error: "Erro ao guardar definições.",
+    });
+  }
 
-        // TODO: Show success message
-    }
-
-
-    return (
-        <Card id="notifications">
-        <CardHeader>
-          <CardTitle>Notificações</CardTitle>
-          <CardDescription>
-            Gere as tuas notificações.
-          </CardDescription>
-        </CardHeader>
-        <CardContent>
-          <div className="grid gap-6">
-            <div className="flex items-center space-x-2">
-              <Checkbox id="new-initiatives" checked={settings.new_initiatives} onCheckedChange={() => setSettings({...settings, new_initiatives: !settings.new_initiatives})} />
-              <label
-                htmlFor="new-initiatives"
-                className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
-              >
-                Novas Iniciativas
-              </label>
-            </div>
-            <div className="flex items-center space-x-2">
-              <Checkbox id="followed-initiatives" checked={settings.followed_initiatives} onCheckedChange={() => setSettings({...settings, followed_initiatives: !settings.followed_initiatives})} />
-              <label
-                htmlFor="followed-initiatives"
-                className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
-              >
-               Iniciativas Guardadas
-              </label>
-            </div>
+  return (
+    <Card id="notifications">
+      <CardHeader>
+        <CardTitle>Notificações</CardTitle>
+        <CardDescription>Gere as tuas notificações.</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <div className="grid gap-6">
+          <div className="flex items-center space-x-2">
+            <Checkbox
+              id="new-initiatives"
+              checked={settings.new_initiatives}
+              onCheckedChange={() =>
+                setSettings({
+                  ...settings,
+                  new_initiatives: !settings.new_initiatives,
+                })
+              }
+            />
+            <label
+              htmlFor="new-initiatives"
+              className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+            >
+              Novas Iniciativas
+            </label>
           </div>
-        </CardContent>
-        <CardFooter className="border-t p-6">
-          <Button onClick={saveSettings}>Guardar</Button>
-        </CardFooter>
-      </Card> 
-    )
-}
+          <div className="flex items-center space-x-2">
+            <Checkbox
+              id="followed-initiatives"
+              checked={settings.followed_initiatives}
+              onCheckedChange={() =>
+                setSettings({
+                  ...settings,
+                  followed_initiatives: !settings.followed_initiatives,
+                })
+              }
+            />
+            <label
+              htmlFor="followed-initiatives"
+              className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+            >
+              Iniciativas Guardadas
+            </label>
+          </div>
+        </div>
+      </CardContent>
+      <CardFooter className="border-t p-6">
+        <Button onClick={saveSettings}>Guardar</Button>
+      </CardFooter>
+    </Card>
+  );
+};
