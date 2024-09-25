288902f1dada9f93886aae637f532e5c7222b980
Tiago Miguel <66640308+tiagomiguel29@users.noreply.github.com>
2024-09-25 02:17:37 +0100

    Added followed initiatives page link button

--- app/iniciativas/page.tsx
+++ app/iniciativas/page.tsx
@@ -22,6 +22,8 @@ import { ExtendedInitiative, Follow } from "@/types/extended.types";
 import { createClient } from "@/utils/supabase/server";
 import { FollowButton } from "@/components/follow-button";
 import { format } from "date-fns";
+import { BookmarkIcon } from "lucide-react";
+import { Button } from "@/components/ui/button";
 
 export default async function Index({
   searchParams,
@@ -88,7 +90,21 @@ export default async function Index({
       <main className="flex-1 w-full md:w-3/4 lg:w-2/3 xl:w-2/3 flex flex-col gap-6 px-4 py-8 md:px-8 md:py-12">
         <div className="container mx-auto grid gap-8 md:gap-12">
           <div>
-            <h2 className="text-2xl font-bold mb-4">Iniciativas</h2>
+            <div className="flex flex-col md:flex-row items-start md:items-center md:justify-between py-6 gap-y-4">
+              <h2 className="text-2xl font-bold">Iniciativas</h2>
+              {user && (
+                <Button asChild variant={"outline"}>
+                  <Link
+                    href="/iniciativas/following"
+                    className="flex justify-center items-center gap-x-2"
+                  >
+                    Guardadas{" "}
+                    <BookmarkIcon className="w-5 h-5 fill-white text-primary" />
+                  </Link>
+                </Button>
+              )}
+            </div>
+
             <div className="grid gap-4 md:gap-6 md:grid-cols-1">
               {initiatives.map((i) => (
                 <Card key={i.id}>
@@ -97,11 +113,13 @@ export default async function Index({
                       <CardTitle className="text-lg font-bold">
                         {i.title}
                       </CardTitle>
-                      {user && <FollowButton
-                        initiativeId={i.id}
-                        userId={user?.id}
-                        followed={followedInitiatives}
-                      />}
+                      {user && (
+                        <FollowButton
+                          initiativeId={i.id}
+                          userId={user?.id}
+                          followed={followedInitiatives}
+                        />
+                      )}
                     </div>
                   </CardHeader>
                   <CardContent>
@@ -207,4 +225,3 @@ const PartyAuthors = ({ initiative }: { initiative: ExtendedInitiative }) => {
     </p>
   );
 };
-
