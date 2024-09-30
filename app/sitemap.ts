162752e87f19566257fe1fa10017abeb48c56519
Tiago Miguel <66640308+tiagomiguel29@users.noreply.github.com>
2024-09-30 15:25:34 +0100

    Added inititatives to sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,28 +1,43 @@
-import type { MetadataRoute } from 'next'
- 
-export default function sitemap(): MetadataRoute.Sitemap {
-    const defaultUrl = process.env.VERCEL_PROJECT_PRODUCTION_URL
-  ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
-  : "http://localhost:3000";
+import supabase from "@/utils/supabase";
+import type { MetadataRoute } from "next";
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const defaultUrl = process.env.VERCEL_PROJECT_PRODUCTION_URL
+    ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
+    : "http://localhost:3000";
+
+  const { data, error } = await supabase.from("initiatives").select("id");
+
+  let initiatives: MetadataRoute.Sitemap = [];
+
+  if (!error) {
+    initiatives = data.map((initiative) => ({
+      url: `${defaultUrl}/iniciativas/${initiative.id}`,
+      lastModified: new Date(),
+      changeFrequency: "hourly",
+      priority: 0.8,
+    }));
+  }
 
   return [
     {
       url: defaultUrl,
       lastModified: new Date(),
-      changeFrequency: 'hourly',
+      changeFrequency: "hourly",
       priority: 1,
     },
     {
-      url: defaultUrl + '/iniciativas',
+      url: defaultUrl + "/iniciativas",
       lastModified: new Date(),
-      changeFrequency: 'hourly',
+      changeFrequency: "hourly",
       priority: 0.8,
     },
     {
-      url: defaultUrl + '/votacoes',
+      url: defaultUrl + "/votacoes",
       lastModified: new Date(),
-      changeFrequency: 'hourly',
+      changeFrequency: "hourly",
       priority: 0.8,
     },
-  ]
+    ...initiatives,
+  ];
 }
