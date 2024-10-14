a492ed260fef79dbcd518901ac9d7a9c932598a5
Tiago Miguel <66640308+tiagomiguel29@users.noreply.github.com>
2024-10-14 17:07:37 +0100

    Changed vote badge to mui

--- components/vote-result-badge.tsx
+++ components/vote-result-badge.tsx
@@ -1,9 +1,12 @@
-import { Badge } from "./ui/badge";
+import { Chip } from "@mui/material";
 
 export function VoteResultBadge({ vote }: { vote: String | undefined | null }) {
-    if (!vote) return null;
-    const bgColor = vote === "Aprovado" ? "bg-green-500" : "bg-red-500";
-  return <Badge
-  className={bgColor + " h-fit"}
-  >{vote}</Badge>;
+  if (!vote) return null;
+  return (
+    <Chip
+      label={vote}
+      size="small"
+      color={vote === "Aprovado" ? "success" : "error"}
+    />
+  );
 }
