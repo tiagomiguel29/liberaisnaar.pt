acbcbb524ec0e0c073d0642502a19d1440de48ae
Tiago Miguel <66640308+tiagomiguel29@users.noreply.github.com>
2024-10-15 01:33:34 +0100

    Redo of reset password form

--- app/protected/reset-password/page.tsx
+++ app/protected/reset-password/page.tsx
@@ -1,8 +1,14 @@
 import { resetPasswordAction } from "@/app/actions";
 import { FormMessage, Message } from "@/components/form-message";
 import { SubmitButton } from "@/components/submit-button";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { TextField } from "@mui/material";
 
 export default async function ResetPassword({
   searchParams,
@@ -10,29 +16,37 @@ export default async function ResetPassword({
   searchParams: Message;
 }) {
   return (
-    <form className="flex flex-col w-full max-w-md p-4 gap-2 [&>input]:mb-4">
-      <h1 className="text-2xl font-medium">Reset password</h1>
-      <p className="text-sm text-foreground/60">
-        Please enter your new password below.
-      </p>
-      <Label htmlFor="password">New password</Label>
-      <Input
-        type="password"
-        name="password"
-        placeholder="New password"
-        required
-      />
-      <Label htmlFor="confirmPassword">Confirm password</Label>
-      <Input
-        type="password"
-        name="confirmPassword"
-        placeholder="Confirm password"
-        required
-      />
-      <SubmitButton formAction={resetPasswordAction}>
-        Reset password
-      </SubmitButton>
-      <FormMessage message={searchParams} />
-    </form>
+    <div className="w-full sm:w-[550px] flex flex-col gap-12 items-center px-4 py-16">
+      <Card className="w-full">
+        <form className="flex flex-col mx-auto">
+          <CardHeader>
+            <CardTitle>Reset password</CardTitle>
+            <CardDescription>
+              Please enter your new password below.
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+            <div className="flex flex-col gap-4 [&>input]:mb-3">
+              <TextField
+                type="password"
+                name="password"
+                label="New password"
+                required
+              />
+              <TextField
+                type="password"
+                name="confirmPassword"
+                label="Confirm password"
+                required
+              />
+              <SubmitButton formAction={resetPasswordAction}>
+                Reset password
+              </SubmitButton>
+              <FormMessage message={searchParams} />
+            </div>
+          </CardContent>
+        </form>
+      </Card>
+    </div>
   );
 }
